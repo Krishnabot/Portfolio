f9e3648036e38c651bfe67d34ec850fd8ce4f815
Krishna <libraryismyparadise@gmail.com>
2025-02-11 23:44:02 +0900

    Refactor portfolio data structure to use objects for stacks and improve readability

--- src/features/Portfolio/portfolio.jsx
+++ src/features/Portfolio/portfolio.jsx
@@ -15,9 +15,12 @@ const data = [
     github: 'https://github.com/Krishnabot/JavaScript-CapStone',
     demo: 'https://krishnabot.github.io/JavaScript-CapStone/dist/',
     shortDescription:
-    'A daily selection of personalized menus. No accounts or sign-ups are required.',
-    stacks: ['JavaScipt', 'css', 'API'],
-
+      'A daily selection of personalized menus. No accounts or sign-ups are required.',
+    stacks: [
+      { id: 1, name: 'JavaScript' },
+      { id: 2, name: 'css' },
+      { id: 3, name: 'API' },
+    ],
   },
   {
     id: 2,
@@ -26,9 +29,12 @@ const data = [
     github: 'https://github.com/Krishnabot/CoinTrackers',
     demo: 'https://zingy-eclair-341d70.netlify.app/',
     shortDescription:
-    'Updates of crypto coins about their market and health. No accounts or sign-ups required.',
-    stacks: ['React', 'Redux', 'API'],
-
+      'Updates of crypto coins about their market and health. No accounts or sign-ups required.',
+    stacks: [
+      { id: 1, name: 'React' },
+      { id: 2, name: 'Redux' },
+      { id: 3, name: 'API' },
+    ],
   },
   {
     id: 3,
@@ -37,9 +43,12 @@ const data = [
     github: 'https://github.com/Krishnabot/Expense-Tracker',
     demo: 'https://expense-tracker-i4p6.onrender.com/',
     shortDescription:
-    'A mobile web application where users can track their expenses and  manage their budget',
-    stacks: ['Rails', 'Postgresql', 'scss'],
-
+      'A mobile web application where users can track their expenses and  manage their budget',
+    stacks: [
+      { id: 1, name: 'Rails' },
+      { id: 2, name: 'Postgresql' },
+      { id: 3, name: 'scss' },
+    ],
   },
   {
     id: 4,
@@ -48,9 +57,12 @@ const data = [
     github: 'https://github.com/Krishnabot/Capstone-Project-Poetry-Page',
     demo: 'https://krishnabot.github.io/Capstone-Project-Poetry-Page/',
     shortDescription:
-    'A responsive Website providing  information on POETRY SLAM 2020',
-    stacks: ['JavaScript', 'css', 'HTML'],
-
+      'A responsive Website providing  information on POETRY SLAM 2020',
+    stacks: [
+      { id: 1, name: 'JavaScript' },
+      { id: 2, name: 'css' },
+      { id: 3, name: 'HTML' },
+    ],
   },
   {
     id: 5,
@@ -59,9 +71,12 @@ const data = [
     github: 'https://github.com/Krishnabot/Space-Traveler-s-Hub',
     demo: 'https://mellifluous-naiad-7463d1.netlify.app/',
     shortDescription:
-    'A web-app for a company that provides commercial and scientific space travel services.',
-    stacks: ['React', 'Redux', 'API'],
-
+      'A web-app for a company that provides commercial and scientific space travel services.',
+    stacks: [
+      { id: 1, name: 'React' },
+      { id: 2, name: 'Redux' },
+      { id: 3, name: 'API' },
+    ],
   },
   {
     id: 6,
@@ -70,9 +85,12 @@ const data = [
     github: 'https://github.com/manq2010/Book-An-Appointment-Front-End',
     demo: 'https://codershub-8u8o.onrender.com/',
     shortDescription:
-    'A web-app for searching and booking online mentors for various topics.',
-    stacks: ['React', 'Redux', 'Rails'],
-
+      'A web-app for searching and booking online mentors for various topics.',
+    stacks: [
+      { id: 1, name: 'React' },
+      { id: 2, name: 'Redux' },
+      { id: 3, name: 'Rails' },
+    ],
   },
 ];
 
@@ -82,8 +100,7 @@ function Portfolio() {
       <h5>My Recent Works</h5>
       <h2>My Portfolio</h2>
       <div className="container portfolio__container">
-        {
-        data.map(({
+        {data.map(({
           id, image, title, github, demo, shortDescription, stacks,
         }) => (
           <article key={id} className="portfolio__item">
@@ -96,24 +113,25 @@ function Portfolio() {
                   <h5>{shortDescription}</h5>
                   <div className="stacks">
                     {stacks.map((stack) => (
-                      <span className="button btn-primary" key={stack.id}>{stack}</span>
+                      <span className="button btn-primary" key={stack.id}>
+                        {stack.name}
+                      </span>
                     ))}
                   </div>
-
                 </div>
               </div>
             </div>
             <h3>{title}</h3>
-
             <div className="portfolio__item-links">
-              <a href={github} className="btn" target="_blank" rel="noreferrer">Github</a>
-              <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-
+              <a href={github} className="btn" target="_blank" rel="noreferrer">
+                Github
+              </a>
+              <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">
+                Live Demo
+              </a>
             </div>
-
           </article>
-        ))
-      }
+        ))}
       </div>
     </section>
   );
